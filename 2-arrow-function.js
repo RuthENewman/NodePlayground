24ac843c0ef8942ee78626bbbf32eaf8ee4c19bd
Ruth Newman <ruthnewman@gmail.com>
2019-05-23 23:51:18 +0100

    function added

--- 2-arrow-function.js
+++ 2-arrow-function.js
@@ -5,3 +5,12 @@
 const square = x => x * x;
 
 console.log(square(3))
+
+const event = {
+  name: 'Maren Morris concert at Albert Hall',
+  printTickets: function() {
+    console.log('2 tickets for ' + this.name)
+  }
+}
+
+event.printTickets();
