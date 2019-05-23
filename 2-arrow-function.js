d64d2a9f40a189fed84b83622ee96d570b2e7dce
Ruth Newman <ruthnewman@gmail.com>
2019-05-24 00:00:11 +0100

    event function

--- 2-arrow-function.js
+++ 2-arrow-function.js
@@ -7,10 +7,16 @@ const square = x => x * x;
 console.log(square(3))
 
 const event = {
-  name: 'Maren Morris concert at Albert Hall',
-  printTickets: function() {
-    console.log('2 tickets for ' + this.name)
-  }
+  name: 'Maren Morris concert',
+  venue: 'Albert Hall',
+  attendees: ['Ruth', 'Sarah'],
+  printTickets() {
+    console.log(this.attendees.length + ' tickets for ' + this.name + ' at ' + this.venue)
+
+    this.attendees.forEach((attendee) => {
+      console.log(attendee + ' is attending the ' + this.name)
+  })
+}
 }
 
 event.printTickets();
