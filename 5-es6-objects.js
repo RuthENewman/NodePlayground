e6f241a25f0fc1e3736690155647f9b4104d6008
Ruth Newman <ruthnewman@gmail.com>
2019-06-04 15:05:28 +0100

    added es6 destructuring

---
+++ 5-es6-objects.js
@@ -0,0 +1,27 @@
+const name = "Ruth";
+const userAge = 30;
+
+const user = {
+  name,
+  age: userAge,
+  location: 'London'
+}
+
+console.log(user)
+
+const product = {
+  label: 'Scarf',
+  price: 10,
+  stock: 108,
+  salePrice: undefined
+}
+
+// const {label: productLabel, stock, rating = 5} = product
+//
+// console.log("Name: " + productLabel, "Quantity: " + stock, "Rating: " + rating)
+
+const transaction = (type, { label, stock }) => {
+  console.log(type, label, stock)
+}
+
+transaction('order', product)
